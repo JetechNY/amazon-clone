842505ef600f795830845f7aea231155e4ae9bed
JetechNY <sunjetliu@gmail.com>
2021-05-05 16:25:26 -0400

    home

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,25 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-      <p>Home</p>
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/ZTU2NzIwMDEt/ZTU2NzIwMDEt-OWVmYjJkMzQt-w1500._CB670405006_.jpg"
+          alt="bkg img"
+        ></img>
+        <div className="home_row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="home_row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="home_row">
+          {/* Product */}
+        </div>
+      </div>
     </div>
   );
 }

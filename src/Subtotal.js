6b9a55c58134e635b47c9616f8e9eb2379e848a9
JetechNY <sunjetliu@gmail.com>
2021-05-11 12:19:23 -0400

    minor

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -6,7 +6,18 @@ function Subtotal() {
   return (
     <div className="subtotal">
       <CurrencyFormat
-        />
+      renderText={(value) => (
+        <>
+        <p>
+          Subtotal (0 items): <strong>0</strong>
+        </p>
+        <small className="subtotal_gift">
+          <input type="checkbox"/> This order contains a gift
+        </small>
+        </>
+      )}
+      
+      />
     </div>
   )
 }

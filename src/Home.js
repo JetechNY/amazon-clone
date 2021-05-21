8c0b2aab13f34df7a159bb9c2876b859f324f16d
JetechNY <sunjetliu@gmail.com>
2021-05-21 00:15:22 -0400

    minor

--- src/Home.js
+++ src/Home.js
@@ -18,7 +18,6 @@ function Home() {
         <div className="home_row">
           <Product />
           <Product />
-          <Product />
         </div>
         <div className="home_row">
           <Product />

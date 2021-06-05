e71a28b73c948a6cecb0bc751677fcad59587ebd
JetechNY <sunjetliu@gmail.com>
2021-06-05 00:09:49 -0400

    checkout product

--- src/Header.js
+++ src/Header.js
@@ -14,7 +14,7 @@ function Header() {
         <img
           className="header_logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-          alt="Amazon Smile Logo."
+          alt="Amazon Smile Logo"
         />
       </Link>
 

4d47695f2e3648480d2869f48e009af091e3b6de
JetechNY <sunjetliu@gmail.com>
2021-06-24 03:22:11 -0400

    minor

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    // BEM abbc
+    // BEM abb
     <Router>
       <div className="app">
         <Header />
